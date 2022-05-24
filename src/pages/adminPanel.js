8626c475d84cdc12966905ede77e5c9ed0cfb682
sanjay <84283520+lsanjayl@users.noreply.github.com>
2022-05-24 11:23:28 +0530

    INITIAL

---
+++ src/pages/adminPanel.js
@@ -0,0 +1,14 @@
+
+import Navbar from "../components/Navbar"
+const AdminPanel=()=>{
+    return(
+        <div>
+            <Navbar/>
+            <div className="container">
+
+            </div>
+        </div>
+    )       
+}
+
+export default AdminPanel;
